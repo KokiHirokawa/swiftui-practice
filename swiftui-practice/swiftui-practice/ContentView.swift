d6f85154b693c41269474d2534711411b3bddebd
KokiHirokawa <kitakitsune1110@gmail.com>
2019-09-30 12:10:42 +0900

    first commit (create new swiftui project)

---
+++ swiftui-practice/swiftui-practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-practice
+//
+//  Created by KokiHirokawa on 2019/09/30.
+//  Copyright © 2019 KokiHirokawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
